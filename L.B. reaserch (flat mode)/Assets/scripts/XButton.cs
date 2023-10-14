d02bafbb4a7c404a88676b6079aeef6122ab2559
GhostSou1 <samodargaj@gmail.com>
2023-10-14 21:53:45 +0200

    inputText, pop up and close for question app, little changes on closing windows

--- L.B. reaserch (flat mode)/Assets/scripts/XButton.cs
+++ L.B. reaserch (flat mode)/Assets/scripts/XButton.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class XButton : MonoBehaviour
 {
     public GameObject windows;
- 
-    
+    public float exitX;
+    public float exitY;
+    public float exitZ;
+
+
     public void OnClick(){
-        windows.transform.position = new Vector3(-19.42f, 3.89f, 0f) ;
+        windows.transform.position = new Vector3(exitX, exitY, exitZ);
     }
 }
