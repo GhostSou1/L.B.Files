ce178d06adeebe772fca032f032f20723a550824
GhostSou1 <samodargaj@gmail.com>
2023-10-19 20:56:03 +0200

    eh

--- L.B. reaserch (flat mode)/Assets/scripts/Manager.cs
+++ L.B. reaserch (flat mode)/Assets/scripts/Manager.cs
@@ -34,19 +34,43 @@ public class Manager : MonoBehaviour
     void Update(){
 
         if(priority == 1){
-            Debug.Log("One is working");
+            Vector3 pos1 = Window1.transform.position;
+            Vector3 pos2 = Window2.transform.position;
+            Vector3 pos3 = Window3.transform.position;
+            pos1.z = -1;
+            pos2.z = 0;
+            pos3.z = 0;
+            Window3.transform.position = pos3;
+            Window2.transform.position = pos2;
+            Window1.transform.position = pos1;
             priority = 0;
         }else if(priority == 2){
-            Debug.Log("two is working");
+            Vector3 pos1 = Window1.transform.position;
+            Vector3 pos2 = Window2.transform.position;
+            Vector3 pos3 = Window3.transform.position;
+            pos1.z = 0;
+            pos2.z = -1;
+            pos3.z = 0;
+            Window3.transform.position = pos3;
+            Window2.transform.position = pos2;
+            Window1.transform.position = pos1;
             priority = 0;
         }else if(priority == 3){
-            Debug.Log("three is working");
+           Vector3 pos1 = Window1.transform.position;
+            Vector3 pos2 = Window2.transform.position;
+            Vector3 pos3 = Window3.transform.position;
+            pos1.z = 0;
+            pos2.z = 0;
+            pos3.z = -1;
+            Window3.transform.position = pos3;
+            Window2.transform.position = pos2;
+            Window1.transform.position = pos1;
             priority = 0;
         }
     }
 
     public void OnClick(InputAction.CallbackContext context){
-        if(!context.started){
+        if(context.canceled){
             return;
         }
 
@@ -59,6 +83,22 @@ public class Manager : MonoBehaviour
             priority = 2;
         }else if(rayHit.collider.gameObject.tag == "Window_3"){
             priority = 3;
+        }
+
+        while(context.performed){
+            if(rayHit.collider.gameObject.tag == "Window_1"){
+            priority = 1;
+            Vector2 pos15 = Window1.transform.position;
+            pos15 = Input.mousePosition;
+            Window1.transform.position = pos15;
+            priority = 0;
+        }else if(rayHit.collider.gameObject.tag == "Window_2"){
+            priority = 2;
+            Vector2 pos25 = Window2.transform.position;
+            pos25 = Input.mousePosition;
+            Window2.transform.position = pos25;
+            priority = 0;
+        }
         }
     }
 }
